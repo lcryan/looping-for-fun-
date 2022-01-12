22f78e59ea20a6a844f8046432d4d397bcad8021
lcryan <lcryan06@gmail.com>
2022-01-12 10:15:39 +0100

    Final version ready for feedback

--- loops.js
+++ loops.js
@@ -7,45 +7,46 @@
 // ==========================================
 //const call = ["Hoera!"];
 
-/*call = ["Hoera!"];
+call = ["Hoera!"];
 
 for (let i = 0; i < 3; i++) {
     console.log(call[0]);
-}*/
+}
 
 
 // Opdracht 2.
-/*const word1 = ['loop...'];
+
+const word1 = ['loop...'];
 
 for (let i = 0; i < 4; i++) {
     console.log(word1[0]);
     if (i >= 3)
         console.log("klaar!")
-}*/
+}
 
 
 // Opdracht 3.
 
-/*for (i = 0; i < 16; i++) {
+for (i = 0; i < 16; i++) {
     if(i % 2 === 0) {
         console.log(i + " is even");
     }
     else {
         console.log (i + " is oneven");
-    } }
-}*/
+    }
+}
 
 // Opdracht 4.
 
 for (i = 0; i <= 9; i++){
 
-    if(i < 3){
-        console.log(i);
+    if(i <= 2){
+        console.log("" + i);
     }
-    if(i >= 3){
+    else if(i >= 3 && i < 6){
         console.log(">" + i);
     }
-    if(i >= 6) {
+    else {
         console.log(">>" + i);
     }
 }
