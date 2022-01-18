bc6fdf517d81543b2bf134f24777f9b827e52b2f
lcryan <lcryan06@gmail.com>
2022-01-18 09:10:23 +0100

    Hey buddy! Thank you for checking my code :)

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -20,9 +20,10 @@ console.log(numbers);
 // Opdracht 3
 
 const squares = [30, 2, 8, 24, 11];
+let y = 0;
 for (i = 0; i < squares.length; i++) {
     const answer = squares[i] **3
-    squares[i] = "Het volumen van" + "" + squares[i] + "" + "is" + "" + "" + answer;
+    squares[i] = `Het volumen van ${squares[i]} is ${answer}`;
 }
 console.log(squares);
 
